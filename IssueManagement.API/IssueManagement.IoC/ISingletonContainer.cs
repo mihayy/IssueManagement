24128bd71d5daa324551b3f8a3c06bd5ac316ed5
Mihai Popovici <st.mihay@gmail.com>
2016-05-18 22:47:47 +0300

    IssueManagement.IoC: added Windor as IoC container

---
+++ IssueManagement.API/IssueManagement.IoC/ISingletonContainer.cs
@@ -0,0 +1,10 @@
+﻿using Castle.Windsor;
+
+namespace IssueManagement.IoC
+{
+    public interface ISingletonContainer
+    {
+        IWindsorContainer GetContainer { get; }
+        void Initialize(IWindsorContainer container);
+    }
+}
---
+++ IssueManagement.API/IssueManagement.IoC/SingletonContainer.cs
@@ -0,0 +1,35 @@
+﻿using Castle.Windsor;
+
+namespace IssueManagement.IoC
+{
+    public class SingletonContainer : ISingletonContainer
+    {
+        private static volatile SingletonContainer _instance;
+        private static readonly object SyncRoot = new object();
+
+        private SingletonContainer() { }
+
+        public static SingletonContainer Instance
+        {
+            get
+            {
+                if (_instance != null) return _instance;
+                lock (SyncRoot)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new SingletonContainer();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        public IWindsorContainer GetContainer { get; private set; }
+
+        public void Initialize(IWindsorContainer container)
+        {
+            GetContainer = container;
+        }
+    }
+}
