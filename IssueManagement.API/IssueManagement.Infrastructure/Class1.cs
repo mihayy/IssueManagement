b2ddf1984fcf61d24d52697b5cd48f283cb7fdb6
Mihai Popovici <st.mihay@gmail.com>
2016-05-18 22:37:16 +0300

    Included new projects: Data, Infrastructure, IoC, Model.

---
+++ IssueManagement.API/IssueManagement.Infrastructure/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueManagement.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
